e390d9097e37edc9ca89d91118501f8b5149b04f
Bebotron <davidrp25@gmail.com>
2016-12-07 14:41:10 -0600

    Raw

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-import io
+"""
+Author: David Rodriguez Perez
+File: main.py
+Description: Runs main functions in interface between instrument signal and app message.
+             If lab instrument fails, it sends a signal which is then translated into some
+             input by the Raspberry pi which then throws that input at the program. This 
+             input is picked up by detect_signal(), which then returns True (False if
+             nothing is picked up). This then calls send_message(signal), which depending
+             on boolean signal, lets the user know if the lab intrument ran into a problem
+             or ran succesfully (no signal sent).
+Date: 12/7/2016
+"""
 
 def main():
+    signal = detect_signal()
+    send_message(signal)
+
+def detect_signal():
     inp = raw_input("Enter input: ")
     if(inp != ""):
-        print("SOY GAY!")
+        return True
+    return False
 
 main()
